b454fcf1761f8db38eb655d4714634ee8937fd20
davidmettman <45125725+davidmettman@users.noreply.github.com>
2021-10-03 17:56:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function compute() {
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    var total = interest + principal;
 
 }
         
