1609c4df3563198b900c61b0fcf04fc400df6aa4
davidmettman <45125725+davidmettman@users.noreply.github.com>
2021-10-18 16:30:11 -0500

    Adding final comments

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// Function to compute simple interest
 function compute()
 {    
+        //Retrieving input values
         principal = +document.getElementById("principal").value;
         years = +document.getElementById("years").value;
         rateval = +document.getElementById("rate").value;
         year = new Date().getFullYear()+years;
+        
+        //Calculating interest and total (principal + interest)
         interest = principal * rateval / 100 * years;
         total = interest + principal;
+        
+        //Setting the label values
         document.getElementById("deposit").innerText = principal;
         document.getElementById("total").innerText=total;
         document.getElementById("time").innerText=year;
@@ -13,6 +19,7 @@ function compute()
         document.getElementById("irate").innerText=rateval;
 }
 
+//Function to update interest rate
 function updateRate()
 {
     rateval2 = +document.getElementById("rate").value;
