6ea79e6dcb2b47242e1b4c54fb55e775cbef0a6e
davidmettman <45125725+davidmettman@users.noreply.github.com>
2021-10-17 21:53:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 principal = parseInt(document.getElementById("principal").value);
+years = parseInt(document.getElementById("years").value);
+rateval = parseInt(document.getElementById("rate").value);
+year = new Date().getFullYear()+years;
+interest = principal * rateval / 100 * years;
+total = interest + principal;
 
-function compute() {
-        
+function compute()
+{    
         document.getElementById("deposit").innerText = principal;
-      
+        document.getElementById("total").innerText=total;
+        document.getElementById("time").innerText=year;
+        document.getElementById("result").innerText=interest;
+}
+
+function updateRate()
+{
+    document.getElementById("rate_val").innerText=rateval;
 }
        
